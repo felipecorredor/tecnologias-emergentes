9ac0babf3107d97dc39665db91b32ff7a9ccc35c
Felipe Corredor <pipecorredor06@gmail.com>
2023-04-18 23:30:10 -0500

    Initial repository

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,11 +1,10 @@
 // project import
 import dashboard from './dashboard';
-import utilities from './utilities';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, utilities]
+  items: [dashboard]
 };
 
 export default menuItems;
